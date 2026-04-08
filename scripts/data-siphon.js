92d403176bf8b305b6f97ddf64383ad169695a92
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-04-08 15:24:05 -0500

    fix blind message

--- scripts/data-siphon.js
+++ scripts/data-siphon.js
@@ -69,7 +69,6 @@ Hooks.on("createChatMessage", (message) => {
 
         ChatMessage.create({
             blind: true,
-            speaker: ChatMessage.getSpeaker({ alias: "Data Siphon System" }),
             content: gmContent,
             whisper: game.users?.filter(u => u.isGM).map(u => u.id)
         });
