5defdeec204cb97a5b2b16b252cf403cfcef0dec
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-04-10 15:00:57 -0500

    Add console logs for errors

--- scripts/eject-power-cores.js
+++ scripts/eject-power-cores.js
@@ -41,7 +41,13 @@ export function initEjectPowerCoresHooks() {
       // If a replacement was made, update the message source before it saves to the database
       if (updatedContent !== message.content) {
         message.updateSource({ content: updatedContent });
+      } else {
+        console.warn(
+          "Lancer Tech Attack Automation | No updates made to message",
+        );
       }
+    } else {
+      console.warn("Lancer Tech Attack Automation | UUID not found for jammed");
     }
   });
 }
